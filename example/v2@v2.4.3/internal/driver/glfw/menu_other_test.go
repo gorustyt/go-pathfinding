05b2bbd5405d7ff27c8b9be72c80d8140670260d
YangT <1228231049@qq.com>
2024-01-25 23:28:22 +0800

    增加fyne demo

--- example/v2@v2.4.3/internal/driver/glfw/menu_other_test.go
+++
@@ -1,9 +0,0 @@
-//go:build !no_glfw && (!darwin || no_native_menus) && !mobile
-// +build !no_glfw
-// +build !darwin no_native_menus
-// +build !mobile
-
-package glfw
-
-func initMainMenu() {
-}
--- example/v2@v2.4.3/internal/painter/gl/shaders/simple.frag
+++
@@ -1,9 +0,0 @@
-#version 110
-
-uniform sampler2D tex;
-
-varying vec2 fragTexCoord;
-
-void main() {
-    gl_FragColor = texture2D(tex, fragTexCoord);
-}
